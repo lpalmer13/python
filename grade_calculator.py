131e5ac7457197e0ae534866305c3681d357de9e
lpalmer13 <pal20029@byui.edu>
2023-02-01 23:50:43 -0700

    commit

--- grade_calculator.py
+++ grade_calculator.py
@@ -1,8 +1,18 @@
 
 
-grade = input(int('What is your grade percentage? '))
-if grade >= 90
-if grade >= 80
-if grade >= 70
-if grade >= 60
-if grade < 60
+grade = int(input('What is your grade percent? '))
+if grade >= 90:
+    letter = 'A'
+elif grade >= 80:
+    letter = 'B'
+elif grade >= 70:
+    letter = 'C'
+elif grade >= 60:
+    letter = 'D'
+else:
+    letter = 'F'
+print(f'Your letter grade is: {letter}')
+if grade >= 70:
+    print("Congratulations! You passed the class!")
+else:
+    print("Stay focused and you'll get it next time!")
---
+++ practicing_if_statements.py
@@ -0,0 +1,20 @@
+first = int(input('What is the first number? '))
+second = int(input('What is the second number? '))
+if first > second:
+    print('The first number is greater')
+else:
+    print('The first number is not greater')
+if first == second:
+    print('The numbers are equal')
+else:
+    print('The numbers are not equal')
+if second > first:
+    print('The second number is greater')
+else:
+    print('The second number is not greater')
+print()
+animal = input('What is your favorite animal? ')
+if animal.lower() == "bear":
+    print("That's my favorite animal too!")
+else:
+    print("That one is not my favorite. ")
