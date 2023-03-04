fd7496a409d23ef0af6384353689d92706a6ed28
lpalmer13 <pal20029@byui.edu>
2023-03-04 16:03:44 -0700

    commit

---
+++ wordle copy.py
@@ -0,0 +1,30 @@
+print('Welcome to the word guessing game!')
+print()
+print('Your hint is: _ _ _ _ _ _ ')
+
+word = 'mosiah'
+guess = '_ _ _ _ _ _'
+number_of_letters = len(word)
+count = 0
+while guess != word:
+    print()
+    guess = input('What is your guess? ').lower()
+    # if guess != word:
+    #     print('Your guess was not correct.')
+    #     count += 1
+    # elif guess == word:
+    #     print('Your guess was correct.')
+    #     count += 1
+
+    for i in range(number_of_letters):
+        w = word[i]
+        g = guess[i]
+        if word[i] == guess[i]:
+            print(word[i].upper(), end=' ')
+        elif g in word:
+            print(g.lower(), end=' ')
+        else:
+            print('_', end=' ')
+print()
+print(f'Congratulations! You guessed it in {count} times!')
+    
