639507f1b323b59e039e6e78b2c94ad2129dc43f
lpalmer13 <pal20029@byui.edu>
2023-03-18 18:53:27 -0600

    commit

--- 11.prove.py
+++ 11.prove.py
@@ -1,6 +1,5 @@
-# year = int(input('Enter the year of interest: '))
 with open("python/life-expectancy.csv") as data:
-    # header = data.readline
+    header = data.readline
     for line in data:
         line = line.strip()
         items = line.split(',')
@@ -8,6 +7,4 @@ with open("python/life-expectancy.csv") as data:
         code = items[1]
         year = items[2]
         life_expectancy = items[3]
-        max_lifexpectancy = max(items[3])
         print(f'{entity} {code} {year} {life_expectancy}')
-    print(f'{max_lifexpectancy}')
