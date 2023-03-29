662082255f703965d0dcb1b386e3153560051403
lpalmer13 <pal20029@byui.edu>
2023-03-29 17:26:34 -0600

    commit

--- areas_of_shapes_revisited.py
+++ areas_of_shapes_revisited.py
@@ -1,3 +1,5 @@
+import math
+
 def compute_area_square(square):
     area = square ** 2
     return area
@@ -16,3 +18,34 @@ length = float(input('What is the length of the rectangle? '))
 width = float(input('What is the width of the rectangle? '))
 area_rectangle = compute_area_rectangle(length, width)
 print(area_rectangle)
+
+def compute_area_circle(radius):
+    area = 3.14 * (radius ** 2)
+    return area
+
+radius = float(input('What is the radius of the circle? '))
+area_circle = compute_area_circle(radius)
+print(area_circle)
+
+shape = ''
+
+while shape != 'quit':
+    shape = input('What shape do you have? ')
+
+    shape = shape.lower()
+
+    if shape == 'square':
+        side = float(input('What is the length of a side of the square? '))
+        area_square = compute_area_square(side)
+        print(f'The area is {area_square}')
+
+    if shape == 'rectangle':
+        length = float(input('What is the length of the rectangle? '))
+        width = float(input('What is the width of the rectangle? '))
+        area_rectangle = compute_area_rectangle(length, width)
+        print(f'The area is {area_rectangle}')
+
+    if shape == 'circle':
+        radius = float(input('What is the radius of the circle? '))
+        area_circle = compute_area_circle(radius)
+        print(f'The area is {area_circle}')
